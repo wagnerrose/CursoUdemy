948414ff4220cde1f24358482173d20e51cca1e9
wagner rose <wagner_rose@yahoo.com.br>
2018-06-11 17:07:32 -0300

    Adding devise users

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ require 'api_version_constraint'
 
 Rails.application.routes.draw do
 
+  devise_for :users
   namespace :api, defaults: { format: :json}, constraints: { subdomain: 'api'}, path: "/" do
     ### configuração de versionamento
     namespace :v1, path: "/", constraints: ApiVersionConstraint.new(version: 1, default: true) do
